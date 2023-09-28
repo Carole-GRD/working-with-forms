4daa4423fdeb9d66898e942aa73e4563825f6025
Carole Gérard <gerard.carole78@gmail.com>
2023-09-28 10:59:17 +0200

    récupérer la valeur d'un input et afficher ou masquer une section en fonction de la valeur

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ firstnameInput.addEventListener('keyup', display);
 function display() {
     firstnameDisplay.textContent = firstnameInput.value;
 }
+
+
+// Add a keyup listener on the second input (the number input), so that if the age is below 18 the content of the section a-hard-truth remains hidden, otherwise show them this hard to swallow fact.
+
+let age = document.querySelector('#age');
+let aHardTruth = document.querySelector('#a-hard-truth');
+
+age.addEventListener('keyup', aHardTruthDisplay);
+
+function aHardTruthDisplay() {
+
+    if (age.value < 18) {
+        aHardTruth.setAttribute('style', 'visibility: hidden');
+    }
+    else {
+        aHardTruth.setAttribute('style', 'visibility: visible');
+    }
+}
